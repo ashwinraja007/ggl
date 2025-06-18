cbd381243ce2d06d6abd980b6dd6e464727a1b84
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-06-18 12:51:15 +0530

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -114,7 +114,15 @@ const Services = () => {
     image: "/lovable-uploads/liquid.jpg",
     description: "Safe and efficient transport solutions for liquid cargo.",
     link: "/services/liquid-transportation"
-  }];
+  }, {
+      id: 5,
+      icon: <Warehouse className="w-5 h-5" />,
+      title: "Project Cargo",
+      image: "/projectcargo3.png",
+      description: "We specialize in delivering end-to-end logistics solutions for complex, heavy, and oversized shipments—commonly known as project cargo.",
+      link: "/services/project-cargo"
+    }
+];
   
   return (
     <div className="min-h-screen flex flex-col">
