31263bb9724c0872dfa31bb4b3f45306582d9c81
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-11-30 18:04:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import TermsOfUse from "./pages/TermsOfUse";
-import BangladeshHome from "./pages/BangladeshHome";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import NotFound from "./pages/NotFound";
+
 import LiquidTransportation from "./pages/services/LiquidTransportation";
 import ProjectCargo from "./pages/services/ProjectCargo";
 import AirFreight from "./pages/services/AirFreight";
 import OceanFreight from "./pages/services/OceanFreight";
 import CustomsClearance from "./pages/services/CustomsClearance";
+
 import GlobalPresence from "./pages/GlobalPresence";
+import BangladeshHome from "./pages/BangladeshHome";
+
 import { ScrollToTop } from "./components/common/ScrollToTop";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 
-// Create a new QueryClient instance
 const queryClient = new QueryClient();
 
-// App component as a regular function component
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -32,6 +33,7 @@ function App() {
         <BrowserRouter>
           <ScrollToTop />
           <Routes>
+            {/* Main site */}
             <Route path="/" element={<Index />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
@@ -39,18 +41,31 @@ function App() {
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/terms-and-conditions" element={<TermsOfUse />} />
             <Route path="/global-presence" element={<GlobalPresence />} />
+
+            {/* Admin */}
             <Route path="/admin" element={<AdminDashboard />} />
+
             {/* Service specific pages */}
-            <Route path="/services/liquid-transportation" element={<LiquidTransportation />} />
+            <Route
+              path="/services/liquid-transportation"
+              element={<LiquidTransportation />}
+            />
             <Route path="/services/air-freight" element={<AirFreight />} />
             <Route path="/services/project-cargo" element={<ProjectCargo />} />
             <Route path="/services/ocean-freight" element={<OceanFreight />} />
-            <Route path="/services/customs-clearance" element={<CustomsClearance />} />
-            <Route path="*" element={<Index  />} />
-            {/* Bangladesh mini-site */}
-            <Route path="/bangladesh" element={<BangladeshHome />} />
-            
+            <Route
+              path="/services/customs-clearance"
+              element={<CustomsClearance />}
+            />
+
+            {/* Bangladesh mini-site 
+               Use /* to allow nested routes inside BangladeshHome if you add them later */}
+            <Route path="/bangladesh/*" element={<BangladeshHome />} />
+
+            {/* Fallback */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
+
           <Toaster />
           <Sonner />
         </BrowserRouter>
